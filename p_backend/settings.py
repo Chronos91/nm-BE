8249e0dfb8e1e4e829c8a4d0e4d16c88b6f4e5bf
Chronos <victorabuke@gmail.com>
2024-10-18 11:14:04 -0500

    a

--- p_backend/settings.py
+++ p_backend/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['https://nm-alpha.vercel.app/']
 
 ROOT_URLCONF = 'p_backend.urls'
 
