6709bebee877ff8a6c5e9df24ceedb8f99daa6e6
Chronos <victorabuke@gmail.com>
2024-10-18 10:36:04 -0500

    pip

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+python --version
+pip --version
+python -m ensurepip --upgrade
+python -m venv venv
+source venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
